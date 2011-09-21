25843d46502c1280b468f318208a0009e66abbce
jmathew <jm8thew@gmail.com>
2011-09-21 16:05:51 -0400

    Maintains state during rotation. Screen resizes button text on rotation as well now

--- src/com/jeff/GridButton.java
+++ src/com/jeff/GridButton.java
@@ -2,17 +2,37 @@ package com.jeff;
 
 import android.widget.Button;
 import android.content.Context;
+import android.util.TypedValue;
+import android.graphics.Canvas;
+import android.view.Display;
+import android.view.WindowManager;
+import android.view.Surface;
 
 public class GridButton extends Button {
 	//getter and setter methods be damned
 	public int status;	
 	public int row;	//position in the grid, zero is first
 	public int col;
+	Display d;
+
 
 	public GridButton(Context con,int row, int col) {
 		super(con);
 		status = 0;
 		this.row = row;
 		this.col = col;
+		this.d = ((WindowManager) con.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+	}
+	@Override
+	public void onDraw(Canvas c) {
+		super.onDraw(c);
+		
+		int ort = d.getRotation();
+
+		if (ort == Surface.ROTATION_90 || ort == Surface.ROTATION_270) {//landscape
+			this.setTextSize(TypedValue.COMPLEX_UNIT_FRACTION,55);
+		}else {//portrait
+			this.setTextSize(TypedValue.COMPLEX_UNIT_FRACTION,80);
+		}
 	}
 }
--- src/com/jeff/tictactoe.java
+++ src/com/jeff/tictactoe.java
@@ -25,37 +25,43 @@ public class tictactoe extends Activity implements View.OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
 	
-		tl = new TableLayout(this);//table layout
+		if (getLastNonConfigurationInstance() == null) {//if first time
+			//set up main screen
+			tl = new TableLayout(this);//table layout
+			FrameLayout.LayoutParams tlp = new FrameLayout.LayoutParams( ViewGroup.LayoutParams.FILL_PARENT,ViewGroup.LayoutParams.FILL_PARENT);
+			tl.setLayoutParams(tlp);
+			tl.setStretchAllColumns(true);
+			
+			//table grids	
+			tr = new TableRow[gridSize];
 		
-		FrameLayout.LayoutParams tlp = new FrameLayout.LayoutParams( ViewGroup.LayoutParams.FILL_PARENT,ViewGroup.LayoutParams.FILL_PARENT);
-		
-		tl.setLayoutParams(tlp);
-		tl.setStretchAllColumns(true);
-		
-		setContentView(tl);
-		
-		//grid
-		grid = new GridButton[gridSize][gridSize];
-		tr = new TableRow[gridSize];
-	
-		//!! children must have layout params of their parent
-		//layout params for rows
-		TableLayout.LayoutParams rl = new TableLayout.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT,1.0f);
-		//layout for cells in the row
-		TableRow.LayoutParams cl = new TableRow.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT,1.0f);
+			/*children must have layout params of their parent */
+			//layout params for rows
+			TableLayout.LayoutParams rl = new TableLayout.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT,1.0f);
+			//layout for cells in the row
+			TableRow.LayoutParams cl = new TableRow.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT,1.0f);
 
-		//loop to create cells
-		for (int i=0;i<=gridSize-1;i++) {
-			tr[i] = new TableRow(this);	
-			for (int j=0;j<=gridSize-1;j++) {
-				grid[i][j] = new GridButton(this,i,j);
-				grid[i][j].setTextSize(120);
-				grid[i][j].setOnClickListener(this);
-				tr[i].addView(grid[i][j],cl);
+			grid = new GridButton[gridSize][gridSize];
+			//loop to create cells
+			for (int i=0;i<=gridSize-1;i++) {
+				tr[i] = new TableRow(this);	
+				for (int j=0;j<=gridSize-1;j++) {
+					grid[i][j] = new GridButton(this,i,j);
+					grid[i][j].setOnClickListener(this);
+					tr[i].addView(grid[i][j],cl);
+				}
+				tl.addView(tr[i],rl);
 			}
-			tl.addView(tr[i],rl);
+
+		}else if (getLastNonConfigurationInstance() != null) {//on restore
+			tl = (TableLayout) getLastNonConfigurationInstance();
 		}
+
+		setContentView(tl);
+
 	}
 
 	public void onClick(View v) {
@@ -75,7 +81,17 @@ public class tictactoe extends Activity implements View.OnClickListener {
 			grid[gb.row][gb.col].setText("O");	
 			currentPlayer = 1;
 		}
-
-
 	}
+	//save state on rotate
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (tl != null) {//save table layout
+			ViewGroup vg = (ViewGroup) tl.getParent();//probably not good
+			vg.removeView(tl);
+			return(tl);
+		}
+		return super.onRetainNonConfigurationInstance();
+	}
+
+
 }
